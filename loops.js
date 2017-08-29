9ac9c514ea02aa9e7ed8daf1bfb94b9676f4cb57
Adam Walter <adam.iverson.walter@gmail.com>
2017-08-29 21:19:04 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,6 +23,6 @@ return Math.random() >= 0.5
 function doWhileLoop(array) {
   do {
     array.pop();
-  } while (array.length > 0 && );
+  } while (array.length > 0 && maybeTrue());
   return array
 }
